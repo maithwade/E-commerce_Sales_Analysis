8f0d258b6bc8358a9d6b6b028b2f731876575462
Maithili Wade <129838254+maithwade@users.noreply.github.com>
2026-05-08 23:49:50 -0400

    sql queries file

--- ecommerce_sales_analysis.sql
+++ ecommerce_sales_analysis.sql
@@ -81,4 +81,92 @@ ORDER BY Revenue DESC
 LIMIT 1;
 
 -- GEOGRAPHIC ANALYSIS
--- 
+-- Which city orders the most 
+SELECT `ship-city`, COUNT(`Order ID`) AS Count
+FROM amazon_sales_report
+GROUP BY `ship-city`
+ORDER BY Count DESC
+LIMIT 1;
+
+-- Which state orders the most 
+SELECT `ship-state`, COUNT(`Order ID`) AS Count
+FROM amazon_sales_report
+GROUP BY `ship-state`
+ORDER BY Count DESC
+LIMIT 1;
+
+-- Which states prefer B2B or regular orders
+SELECT `ship-state`, B2B, COUNT(`Order ID`) AS Count
+FROM amazon_sales_report
+WHERE B2B = 'True'
+GROUP BY `ship-state`, B2B
+ORDER BY Count DESC
+LIMIT 5;
+
+-- Order Status Analysis
+-- What % of orders were cancelled?
+SELECT COUNT(`Order ID`) AS total_orders,
+	SUM(CASE WHEN Status = 'Cancelled' THEN 1 ELSE 0 END) AS cancelled_orders, 
+    ROUND((SUM(CASE WHEN Status = 'Cancelled' THEN 1 ELSE 0 END) / COUNT(`Order ID`)) * 100, 2) AS percentage
+FROM amazon_sales_report;
+
+-- Which fulfilment method has the most cancellations?
+SELECT fulfilment, COUNT(Status) AS Count
+FROM amazon_sales_report
+WHERE Status = 'Cancelled'
+GROUP BY fulfilment
+ORDER BY Count DESC
+LIMIT 1;
+
+-- Shipping Analysis
+-- Which courier status is most common?
+SELECT `Courier Status`, COUNT(`Courier Status`) AS Count
+FROM amazon_sales_report
+GROUP BY `Courier Status`
+ORDER BY COUNT DESC;
+
+-- Standard VS Expedited Shipping?
+SELECT `ship-service-level`, COUNT(`ship-service-level`) AS Count
+FROM amazon_sales_report
+GROUP BY `ship-service-level`
+ORDER BY Count DESC;
+
+-- Revenue Analysis 
+-- Average Order Value Per category
+SELECT Category, ROUND(AVG(Amount), 2) AS avg_amt
+FROM amazon_sales_report
+GROUP BY Category;
+
+-- category having the highest average sale amount
+SELECT Category, ROUND(AVG(Amount), 2) AS avg_amt
+FROM amazon_sales_report
+GROUP BY Category
+HAVING avg_amt > 500;
+
+SELECT COUNT(*) FROM amazon_sales_report
+WHERE Date IS NOT NULL;
+
+SELECT 
+    DATE_FORMAT(STR_TO_DATE(Date, '%m-%d-%y'), '%Y-%m-%d') AS Date,
+    `Order ID`,
+    Status,
+    Fulfilment,
+    `Sales Channel`,
+    `ship-service-level`,
+    Style,
+    SKU,
+    Category,
+    Size,
+    ASIN,
+    `Courier Status`,
+    Qty,
+    currency,
+    Amount,
+    `ship-city`,
+    `ship-state`,
+    `ship-postal-code`,
+    `ship-country`,
+    `promotion-ids`,
+    B2B,
+    `fulfilled-by`
+FROM amazon_sales_report;
